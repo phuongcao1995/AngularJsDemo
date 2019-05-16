6db8c659facbf297b60b58edab580514882cc2df
drake <caongocphuong95@gmail.com>
2019-05-16 10:24:09 +0700

    add file commonservice.js

---
+++ CasePortal/Scripts/commonService.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var app = angular.module('App');
+    // service - use $http for http requests, $q for promises
+    app.factory('commonService', ['$http', '$q', '$rootScope', function ($http, $q, $rootScope) {
+        var service = {
+            StartLoading: StartLoading,
+            EndLoading: EndLoading
+        };
+        return service;
+
+        function StartLoading() {
+            $rootScope.loading = true;
+        }
+        function EndLoading() {
+            $rootScope.loading = false;
+        }
+    }]);
+})();
